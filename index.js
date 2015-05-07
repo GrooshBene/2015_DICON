d4678f1967811ae6f93498a801c1f78d7b3187d1
GrooshBene <pi@grooshbene.(none)>
2015-05-07 01:02:25 +0000

    Server Details Changed, Express Module Added

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var io = require('socket.io').listen(8000);
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+server.listen(80);
+var io = require('socket.io')(server);
+console.log('Server Running At 80');
 io.sockets.on('connection',function(socket){
   socket.emit('connectionChk',{
     open: "Server Opened."
   });
-  socket.on('prsList',Function(data){
+  socket.on('prsList',function(data){
     console.log(data);
   });
-})
+});
+app.get("/", function(req,res){
+	res.sendfile("index.html");
+});
